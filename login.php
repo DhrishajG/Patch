f89938b8cc5e7691b6e7d075e30861e5bb5f12d8
d66835dg <dhrishaj.garg@student.manchester.ac.uk>
2022-03-13 15:06:11 +0000

    testing database connection

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php
+
+$database_host = "dbhost.cs.man.ac.uk";
+$database_user = "d66835dg";
+$database_pass = "KillBurn";
+$database_name = "2021_comp10120_z6";
+
+try
+{
+  $conn = new PDO("mysql:host=$databse_host", $database_user, $database_pass);
+  echo("Connection successful");
+}
+catch(PDOException $pe)
+{
+  die("Could not connet to $host :" . $pe->getMessage());
+}
+
+
+?>
