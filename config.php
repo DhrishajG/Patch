c0c5f7be2ee3b1e7efedb539b070b39e33f53f72
d66835dg <dhrishaj.garg@student.manchester.ac.uk>
2022-03-17 14:21:17 +0000

    added login page

--- config.php
+++ config.php
@@ -8,12 +8,10 @@ $database_name = "2021_comp10120_z6";
 try
 {
   $conn = new PDO("mysql:host=$database_host", $database_user, $database_pass);
-  echo("Connection successful");
 }
 catch(PDOException $pe)
 {
   die("Could not connet to $database_host :" . $pe->getMessage());
 }
-
-
+}
 ?>
